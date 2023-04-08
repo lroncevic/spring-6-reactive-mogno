863916ee9836e53b702c2c507495b81cf1c65cec
lroncevic <roncevicluka@outlook.com>
2023-04-08 14:20:35 +0200

    Added mongo credentials

--- src/main/java/com/lukaroncevic/reactivemongo/config/MongoConfig.java
+++ src/main/java/com/lukaroncevic/reactivemongo/config/MongoConfig.java
@@ -1,10 +1,17 @@
 package com.lukaroncevic.reactivemongo.config;
 
+import com.mongodb.MongoClientSettings;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
 import com.mongodb.reactivestreams.client.MongoClient;
 import com.mongodb.reactivestreams.client.MongoClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration;
+
+import java.util.Collections;
+
+import static java.util.Collections.singletonList;
 
 @Configuration
 public class MongoConfig extends AbstractReactiveMongoConfiguration {
@@ -19,5 +26,13 @@ public class MongoConfig extends AbstractReactiveMongoConfiguration {
         return "mongodb";
     }
 
-
+    @Override
+    protected void configureClientSettings(MongoClientSettings.Builder builder) {
+        builder.credential(MongoCredential.createCredential("root",
+                "admin", "example".toCharArray()))
+                .applyToClusterSettings(settings -> {
+                    settings.hosts(singletonList(new ServerAddress("127.0.0.1", 27017)
+                    ));
+                });
+    }
 }
